f713386f73f708661dbf46ad0c59c645ca8cac47
JoaoFerreiraPcPositivo <joaovitor13cv@hotmail.com>
2022-08-13 17:33:58 -0300

    Adicionado SHEBANG e alguns comentários

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+
+# Uma boa pratica adicionar shebang no script
+# Maneira mais recomendada e usar o ambiente virtual
+
+# Este programa imprime Hello World
 print("Hello, World!")
